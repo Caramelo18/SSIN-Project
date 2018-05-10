a119dec8d8cf9094a03392af9a61ca685958dd10
BrunoBarros21 <brunocostabarros@hotmail.com>
2018-05-11 00:43:57 +0100

    Server initiated

--- server.py
+++ server.py
@@ -1,5 +1,29 @@
-def main():
-  print('Hello world! - server')
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import ssl
 
-if __name__ == '__main__':
-  main()
+from io import BytesIO
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(b'This is POST request. ')
+        response.write(b'Received: ')
+        response.write(body)
+        self.wfile.write(response.getvalue())
+
+
+httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket (httpd.socket, keyfile="./key.pem", certfile='./cert.pem', server_side=True,
+                               ssl_version=ssl.PROTOCOL_TLSv1)
+
+httpd.serve_forever()
